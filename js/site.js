3e4237dc7e630fee446fea01df87fd5a73ea788b
Jason Bright <jbright.dev@gmail.com>
2022-07-29 03:47:33 -0500

    Catch Up Commit

--- js/site.js
+++ js/site.js
@@ -72,7 +72,7 @@ buildDD();
 /*Declare a new function named buildDD*/
 function buildDD(){
 
-  /*Withing the scope-block of buildDD create a variable named "eventDD" which will be defined as the value of the document-element possessing the id of "eventDropDown*/
+    /*Withing the scope-block of buildDD create a variable named "eventDD" which will be defined as the value of the document-element possessing the id of "eventDropDown*/
   let eventDD = document.getElementById("eventDropDown");
   
   /*When called, the innerHTML of the eventDD item will have the value of an empty text string*/
@@ -89,6 +89,9 @@ function buildDD(){
 
   /*When called, the content of the ddItem will have the value of "All": we now have <a>All</a>*/
   ddItem.textContent = "All";
+
+  /*When called, the attribute of the ddItemNode will be set to "data-string"; "data- creates a special array of variable custom attributes; The name of the array will be "All"; This acts as a filter*/
+  ddItem.setAttribute("data-string", "All");
 
   /*When called the child of the "eventDD" item will be appended with the value of "ddItemNode": We now have <li><a>All</a></li>*/
   eventDD.appendChild(ddItemNode);
@@ -118,6 +121,9 @@ function buildDD(){
     /*When called the content of the "ddItem" will have the value of the variable "cityName*/
     ddItem.textContent = cityName;
 
+    /*When called, the attribute of the ddItemNode will be set to "data-string" which will The name of the array will have the values of the variable cityName*/
+    ddItem.setAttribute("data-string", cityName);
+
     /*When called, the child of the "eventDD" item will be appended with the value of "ddItemNode", defined within the scope of this loop"*/
     eventDD.appendChild(ddItemNode);
 
@@ -132,15 +138,17 @@ function buildDD(){
   
 function displayStats(currentEvents){
   
-  /*Initialize the variables, total, average, most, least, and give them a value of 0*/
+  /*Initialize the variables, total, average, most,and give them a value of 0, initialize a variable of least and give it a value
+  of the first value of attendance within the "currentEvents" array, which will then be set to zero*/
   //Ref. JS Operators//
 
   let total = 0;
   let average = 0;
   let most = 0;
-  let least = 0;
+  let least = currentEvents[0].attendance;
 
-  /*Create a for-loop where index has a value of 0. As long as the value of index is less than the length of the "currentEvents array, add one to the value of index" */
+  /*Create a for loop where a variable of index" will have a value of zero; As long as the value of "index" is less than the length of the currentEvents array
+  add one to it*/
   for (let index = 0; index < currentEvents.length; index++) {
 
     /*Within the scope of the for-loop initialize a variable named "attendance" which will the value of attendance at the "currentEvents" index*/
@@ -150,7 +158,65 @@ function displayStats(currentEvents){
     /*add the value of "total" to the vaule of attendance*/
     total += attendance;
 
-    
-    
+    /* create an if-statement where, if the value of "most" is less than the value of "attendance", within the scope of the statement, the value of most will be the value of attendance*/
+    if(most < attendance){
+      most = attendance;
+    }
+
+    /*Create an if-statement where, if the value of least is greater than the value of attendance or (||) if the value of least is less than zero, the value of least will be the value of attendance*/
+    if(least > attendance){
+      least = attendance
+    }
+      
   }
+
+  /*When called, the value of avearge has the value of the previously defined variable "total" divided by the length of the currentEvents array*/
+  average = total / currentEvents.length;
+
+  //Now we must display our calcluations// //MLC//
+
+  /*When called, the document elements with the id's of total, most, least, and average will have the value of their respective previously defined variables, and
+  will be formatted using "toLocaleString()" function, which formats the string as a number based on the users locale */
+
+  document.getElementById("total").innerHTML = total.toLocaleString();
+  document.getElementById("most").innerHTML = most.toLocaleString();
+  document.getElementById("least").innerHTML = least.toLocaleString();
+
+  /*In the case of average, the locale string will be english, US and the minimum and maximum fractional digits allowed will be 0*/
+  document.getElementById("average").innerHTML = average.toLocaleString("en-US",{
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  }
+  );
+
+}
+
+//---Get the events---//
+/*Declare a new function named "getEvents"*/
+/*the parameter "elements" is used in this function because the keyword "this" was used in the html*/
+function getEvents(element){
+
+  /*Within the scope of the function, initialize a varaible which will be defined as the element with the data attribute of "data-string"; this will filter the array by the city name*/
+  let city = element.getAttribute("data-string");
+}
+
+//now we will store events into local storage
+
+/*Declare a new function named "getEventData"*/
+function getEventData(){
+
+  /*Initialize a variable named "currentEvents" which will have a value of will parse the local data for items with the id of "eventData", which will be then converted into JSON  */
+  let currentEvents = JSON.parse(localStorage.getItem("eventData"));
+
+  /*Create an if statement where if current events has a value of null*/
+  if(currentEvents == null){
+    /*Within the scope of the statement, the variable currentEvents will have the value of the object "events"*/
+    currentEvents = events;
+
+    /*the local data item with the id of "eventData", will be set to the value of the varialbe "currentEvents" which has been converted to JSON via stringification*/
+    localStorage.setItem("eventData",JSON.stringify(currentEvents));
+  }
+  
+  return currentEvents;
+  
 }
